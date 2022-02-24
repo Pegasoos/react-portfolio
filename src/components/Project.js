33ab7a21f28642be46f6d0ac9dc9d9275bf7e82d
Pegasoos <circumambulation24@yahoo.com>
2022-02-23 20:29:38 -0500

    Project component done

---
+++ src/components/Project.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Project(props){
+    return(
+<div className="col-sm-6 col-md-12 col-lg-6">
+        <h2>Tech Blog</h2>
+        <a href={props.liveurl}>
+        <figure className="figure">
+          <img src={props.imgsrc} className="figure-img img-fluid rounded border border-dark portfolio-image" alt="..."/>
+        </figure>
+        </a>
+        <p>
+          <a href={props.github}><i className="fab fa-github"></i> Github Repository</a>
+        </p>
+      </div>
+      )
+};
+
+export default Project
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import Project from '../Project';
 
 function Portfolio(){
-
+return(
+<div className="container">
+  <section className="row">
+    <div className="col-md-8 block rounded">
+      <article className="portblock">
+        <h1>Portfolio</h1>
+        <hr></hr>
+      </article>
+    </div>
+</section>
+<div className="row">
+      <Project />
+      <Project />
+</div>
+<div className="row">
+      <Project />
+      <Project />
+</div>
+<div className="row">
+      <Project />
+      <Project />
+</div>
+</div>
+)
 }
 
 export default Portfolio;
