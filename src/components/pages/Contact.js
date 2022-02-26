a8ad81344d971aacfd4e3bf2281b42006ce65ad0
Pegasoos <circumambulation24@yahoo.com>
2022-02-25 23:53:16 -0500

    Connected formsubmit to contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,23 +7,27 @@ function Contact(){
     Contact
   </h1> 
 <hr></hr>
+<form action="https://formsubmit.co/fb1081e4e60cc6ebed200a778ffeac99" method="POST">
   <p>
     Name
   </p>
-    <input type="text" placeholder="Name" className="formsize" />
+    <input type="hidden" name="_subject" value="New Contact!"></input>
+
+    <input type="text" placeholder= "Name" name = "name" className="formsize" required/>
   <p class="conpad">
     Email
   </p>
-    <input type="text" placeholder="Email" className="formsize" />      
+    <input type="email" placeholder= "Email" name = "email" className="formsize" required/>      
   <p class="conpad">
     Message
   </p>
-    <textarea placeholder="Message" className="formsize"></textarea>
+    <textarea placeholder="Message" name = "message" className="formsize" required></textarea>
     <div class="conpad">
     <button class="submit">
       Submit
     </button>
     </div>
+</form>
     </article>
     )
 }
